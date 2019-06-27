881fd1efe783472c76f8887b27b8e2839ec24594
Jerry Howard <jerryhoward@fastmail.fm>
2019-06-27 18:39:47 -0400

    Adds messaging for item added. Also adds deletion of items + messaging. Deletions should not be done via GET, but not sweating that just now.

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import List
 from .forms import ListForm
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
@@ -9,6 +11,7 @@ def home(request):
         form = ListForm(request.POST or None)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Item has been added to the list')
 
     # This has to happen regardless of request method, in order to fetch and display the list
     all_items = List.objects.all
@@ -17,3 +20,12 @@ def home(request):
 def about(request):
     context = {'fname': 'Jerry', 'lname': 'Howard'}
     return render(request, 'about.html', context)
+
+def delete(request, list_id):
+    item = List.objects.get(pk=list_id)
+    item.delete() 
+    # ^^ Remember, you're not running a delete operation on a table, but an object representing
+    # a row in the table.
+    messages.success(request, 'Item has been deleted')
+    return redirect('home')
+
