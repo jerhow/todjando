545eaf95fd1cb920813a814a4bf2715ecfebdca7
Jerry Howard <jerryhoward@fastmail.fm>
2019-06-27 16:48:50 -0400

    Basic context passing from views into templates

--- todo_list/views.py
+++ todo_list/views.py
@@ -6,4 +6,5 @@ def home(request):
     return render(request, 'home.html', {})
 
 def about(request):
-    return render(request, 'about.html', {})
+    context = {'fname': 'Jerry', 'lname': 'Howard'}
+    return render(request, 'about.html', context)
